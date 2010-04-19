ea1f14ef7927811ee3721dd56a6f56c2882ad25b
sauger <sauger@163.com>
2010-04-19 20:44:22 +0800

    fix bugs

--- admin/plugin/updatedb.rb
+++ admin/plugin/updatedb.rb
@@ -4,9 +4,10 @@ my = Mysql.connect('192.168.1.4', 'forbes_db','xunao','forbes')
 stmt = my.prepare("delete from fb_dynamic_fortune_list")
 stmt.execute
 index = 0
-my.query("select d.id,d.name,sum(a.stock_value*c.rate*b.stock_count*100) as fortune from fb_company a right join fb_rich_company b on a.id = b.company_id left join fb_currency c on a.hbid= c.id left join fb_rich d on b.rich_id = d.id group by b.rich_id order by fortune desc").each do |id, name, count| 
+my.query("select d.id,d.name,sum(a.stock_value*c.rate*b.stock_count) as fortune from fb_company a right join fb_rich_company b on a.id = b.company_id left join fb_currency c on a.hbid= c.id left join fb_rich d on b.rich_id = d.id group by b.rich_id order by fortune desc").each do |id, name, count| 
 	index += 1	
 	last_index = 0	
+	fortune = fortune.to_i
 	if index <= 100
 		my.query("select current_index from fb_dynamic_fortune_history where richer_id = #{id} order by regdate desc limit 1").each do |last|
 			last_index = last.to_s
