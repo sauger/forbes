d9adf8fa4bdf7daf78583513bbbf9e0a1dfd9a19
sauger <sauger@163.com>
2010-04-19 20:07:23 +0800

    fix bugs

--- lib/Examples/example3.3.php
+++ lib/Examples/example3.3.php
@@ -1,10 +1,10 @@
 <?php // content="text/plain; charset=utf-8"
-require_once ('jpgraph/jpgraph.php');
-require_once ('jpgraph/jpgraph_line.php');
-require_once ('jpgraph/jpgraph_log.php');
+require_once ('../jpgraph/jpgraph.php');
+require_once ('../jpgraph/jpgraph_line.php');
+require_once ('../jpgraph/jpgraph_log.php');
 
 // Some (random) data
-$ydata = array(1,7,8,12,5);#,5,9,13,5,7,10,13,12,13,20,25);
+$ydata = array(1.12,7.12,8.43,12.2,5.3);#,5,9,13,5,7,10,13,12,13,20,25);
 #$xdata = array(1,2,3);
 $datax = array("9:30","10:00","10:30","11:00","11:30","12:00","12:30","13:00","13:30","14:00");
 // Size of the overall graph
@@ -34,7 +34,7 @@ $graph->yaxis->SetColor('red');
 // Create the linear plot
 $lineplot=new LinePlot($ydata,$xdata);
 $lineplot->SetColor( 'blue' );
-$lineplot->SetWeight( 2 );   // Two pixel wide
+#$lineplot->SetWeight( 2 );   // Two pixel wide
 $lineplot->SetCenter();
 $lineplot->mark->SetType(MARK_UTRIANGLE);
 $lineplot->mark->SetColor('blue');
@@ -46,6 +46,6 @@ $lineplot->value->Show();
 $graph->Add($lineplot);
 
 // Display the graph
-#$graph->Stroke();
-$graph->Stroke('../../upload/img.png');
+$graph->Stroke();
+#$graph->Stroke('../../upload/img.png');
 ?>
