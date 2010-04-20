f687beb7abef338d00435bc1499659c82ad335d3
sauger <sauger@163.com>
2010-04-20 11:07:59 +0800

    fix bugs

--- admin/plugin/generate_ipo_image.php
+++ admin/plugin/generate_ipo_image.php
@@ -49,10 +49,11 @@ $lineplot->SetCenter();
 $lineplot->SetWeight( 2 );   // Two pixel wide
 $lineplot->mark->SetType(MARK_FILLEDCIRCLE);
 $lineplot->mark->SetWidth(3);
-$lineplot->mark->SetColor('blue');
+$lineplot->mark->SetColor('red');
 $lineplot->mark->SetFillColor('red');
 
 $lineplot->value->format = "%.1f";
+$lineplot->value->SetFont(FF_FONT1,FS_NORMAL,5);
 $lineplot->value->Show();
 
 // Add the plot to the graph
