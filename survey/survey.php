0a6ed7eee2172383317c0c2660b58b5383a82681
justin <zhanghao@xun-ao.com>
2010-04-21 13:40:28 +0800

    1

--- survey/survey.php
+++ survey/survey.php
@@ -1,12 +1,7 @@
 ﻿<?php 
 	require_once('../frame.php');
-
-		$db = get_db();
-		$id = intval($_GET['id']);
-		if($id==0)
-		{
-				$id=1;	
-		}
+	$db = get_db();
+	$id = intval($_GET['id']);
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
