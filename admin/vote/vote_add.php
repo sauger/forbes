2a25ee77745909e24647af4773621bd40bdf61ec
justin <zhanghao@xun-ao.com>
2010-04-21 16:19:08 +0800

    vote

---
+++ admin/vote/vote_add.php
@@ -0,0 +1,109 @@
+<?php
+	require_once('../../frame.php');
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<meta http-equiv=Content-Language content=zh-CN>
+	<title>smg</title>
+<?php 
+	css_include_tag('admin','thickbox','jquery_ui');
+	use_jquery_ui();
+	js_include_tag('admin_pub','vote','thickbox');
+	validate_form("vote_form");
+?>
+</head>
+
+<body>
+<div id=icaption>
+	<div id=title>添加投票</div>
+	  <a href="index.php" id=btn_back></a>
+</div>
+<form id="vote_form" method="post" enctype="multipart/form-data" action="vote.post.php">
+	<div id=itable>
+ 	<table cellspacing="1" align="center">
+		<tr class=tr4>
+			<td align="center" width="15%">标题：</td>
+			<td width="85%" align="left"><?php show_fckeditor('title','Title',true,"80");?></td>
+		</tr>
+		<tr class=tr3>
+			<td>描述：</td>
+			<td align="left"><textarea cols=70 name="vote[description]"></textarea></td>
+		</tr>
+		<tr class=tr3>
+			<td>添加图片：</td>
+			<td align="left"><input type="hidden" name="MAX_FILE_SIZE" value="2097152"><input name="image" id="image" type="file"></td>
+		</tr>
+		<tr class=tr3>
+			<td>所属类别：</td>
+			<td align="left">
+				<select  name="vote[category_id]">
+					<?php for($i=0;$i<$count;$i++){?>
+					<option value="<?php echo $category_menu[$i]->id;?>"><?php echo $category_menu[$i]->name;?></option>
+					<?php }?>
+				</select>
+			</td>
+		</tr>
+		<tr class=tr3>
+			<td>投票类型：</td>
+			<td align="left">
+				<select id=select_vote_type name="vote[vote_type]">
+					<option value="word_vote">文字投票</option>
+					<option value="image_vote">图片投票</option>
+					<option value="more_vote">复合投票</option>
+				</select>
+			</td>
+		</tr>
+		<tr class=tr3>
+			<td>控制方式：</td>
+			<td align="left">
+				<select id=select_limit_type name="vote[limit_type]">
+					<option value="user_id">工号登录</option>
+					<option value="ip">IP控制</option>
+					<option value="no_limit">不设限制</option>
+				</select>
+			</td>
+		</tr>
+		<tr class=tr3>
+			<td>投票次数限制：</td>
+			<td align="left"><input type="text" name="vote[max_vote_count]" class="number" id="max_vote_count">如果不填则无限制</td>
+		</tr>
+		<tr class=tr3>
+			<td>投票选项限制：</td>
+			<td align="left"><input type="text" name="vote[max_item_count]" class="number" id="max_item_count">如果不填则无限制</td>
+		</tr>
+		<tr class=tr3>
+			<td>开始日期：</td>
+			<td align="left"><input type="text" class="date_jquery" name="started_at" id="start"></td>
+		</tr>
+		<tr class=tr3>
+			<td>截止日期：</td>
+			<td align="left"><input type="text"  class="date_jquery" name="ended_at" id="end"></td>
+		</tr>
+		<tr class=tr3 id="item">
+			<td>投票项目：</td>
+			<td align="left" id="single">
+				标题<input type="text" name="vote_item1[title]" id="first_item" style="width:300px" class="required">
+				<input type="hidden" name="MAX_FILE_SIZE" value="2097152">
+				<input name="item_image1" type="file" class="item_image" style="display:none;">
+				<a id="add_item" value="1" style="cursor:pointer;">继续添加</a>
+				<input type="hidden" name="deleted1" value="false">
+			</td>	
+			<td align="left" style="display:none;" id="many">
+					<a  href="" class="thickbox" id="add_sub_vote">添加子投票</a>
+					<a  id="can_not_add" style="display:none; cursor:pointer;">请先填写日期并选择好控制方式</a>
+			</td>
+		</tr>
+		<tr class=btools>
+			<td colspan="10" align="center"><input id="submit" type="submit" value="发布投票"></td>
+			<input type="hidden" name="vote[created_at]"  value="<?php echo date("y-m-d")?>">
+			<input type="hidden" id="vote_item_count" name="vote_item_count" value="1">
+			<input type="hidden" name="vote[is_sub_vote]" value="0">
+		</tr>
+ 	</table>
+	</div>
+ </form>
+</body>
+</html>
--- survey/survey.php
+++ survey/survey.php
@@ -2,6 +2,14 @@
 	require_once('../frame.php');
 	$db = get_db();
 	$id = intval($_GET['id']);
+	if(empty($id)){
+		redirect('/error.html');
+		die();
+	}
+	$question = $db->query("select * from fb_survey_question where survey_id=$id order by priority");
+	$count = $db->record_count;
+	$item = $db->query("select * from fb_survey_item where survey_id = $id");
+	$item_count = $db->record_count;
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -19,61 +27,35 @@
 </head>
 <body>
 	<div id=ibody>
-		
 		<?php include "../inc/top.inc.php";?>
-		
 		<div id=bread>
-			
-				<span>在线问卷</span>
-				
+			<span>在线问卷</span>
 		</div>
-
 		<div id="hr_top"></div>
-		
 		<div id="questions_div">
 			<div id="questions_top"></div>
-			
 				<div id="questions_n">
-				
 						<div id="question_menu">在线问卷</div>	
 						<div id="questions__top">
-							
-							<div class="answer_title">问题1：</div>
+							<?php for($i=0;$i<$count;$i++){?>
+							<div class="answer_title">问题<?php echo $i+1?>：<?php echo $question[$i]->name;?></div>
 							<div class="answer_dd">
-							<DIV CLASS="answer_z">
-												<DIV CLASS="answer_select"><input type="checkbox" id="checkboxA" name="checkbox" value="A" /></DIV>
-												<div class="answer"><label for="checkboxA">你选sdfsadfsa择的是A</label></div>
-							</DIV>
-							<DIV CLASS="answer_z">
-												<DIV CLASS="answer_select"><input type="checkbox"id="checkboxB" name="checkbox" value="B" /></DIV>
-												<div class="answer"><label for="checkboxB">你选择dfasfas的是</label></div>
-							</DIV>
-							<DIV CLASS="answer_z">
-												<DIV CLASS="answer_select"><input type="checkbox" ID="checkboxC" name="checkbox" value="C" /></DIV>
-												<div class="answer"><label for="checkboxC">你选择fasdfsad的是C</label></div>
-							</DIV>
-							<DIV CLASS="answer_z">
-												<DIV CLASS="answer_select"><input type="checkbox" id="checkboxD" name="checkbox" value="D" /></DIV>
-												<div class="answer"><LABEL for="checkboxD">你选择sdfsafassafdasfsa的是D</label></div>
-							</DIV>
+								<?php for($j=0;$j<$item_count;$j++){
+									if($item[$j]->question_id==$question[$i]->id){
+								?>
+								<div class="answer_z">
+									<div class="answer_select"><input type="radio" id="checkbox<?php echo $item[$j]->id;?>" name="<?php echo $question[$i]->id;?>" value="<?php echo $item[$j]->id;?>" /></DIV>
+									<div class="answer"><label for="checkbox<?php echo $item[$j]->id;?>"><?php echo $item[$j]->name;?></label></div>
+								</div>
+								<?php }}?>
+							</div>
+							<?php }?>
+							<div id="answer_hr"></div>
+							<div id="submit_div"><input type="button" value="提&nbsp;交" id="submit"></div>
 						</div>
-						<div id="answer_hr"></div>
-							<div id="submit_div"><input type="buttom" value="提&nbsp;交" id="submit"></div>
-							
-						</div>
-						
-				
-				
 				</div>
-				
 			<div id="questions_bottom"></div>
-			
-			
-
-			
-		
 		</div>
-		
 		<div id="right_inc">
 		 		<?php include "../right/ad.php";?>
 		 		<?php include "../right/favor.php";?>
@@ -82,7 +64,6 @@
 		 		<?php include "../right/magazine.php";?>
 		</div>
 		<?php include "../inc/bottom.inc.php";?>
-		
 	</div>
 </body>
 <html>
