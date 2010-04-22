c6cf5da41c0f5f65b8bc9a0736cd7984e0eb0cf4
shelby <sunyoujie@xun-ao.com>
2010-04-22 18:40:38 +0800

    shelby1982

--- business/index.php
+++ business/index.php
@@ -1,5 +1,4 @@
 ﻿<?php 
-	session_start();
 	require_once(dirname(__FILE__).'/../frame.php');
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
