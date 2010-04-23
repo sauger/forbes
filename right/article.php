845e4c3bd8ba0ff3b053710261928ca08b0d49cc
justin <zhanghao@xun-ao.com>
2010-04-23 13:45:36 +0800

    right fix

--- right/article.php
+++ right/article.php
@@ -1,10 +1,10 @@
-<div id=r_t_title>
-	<div id=wz>福布斯精华文章定制</div>	
+<div class=right_title>
+	<div class=title_con>福布斯精华文章定制</div>	
 </div>
 <div class="right_box">
 	<div id=order_top>
 		<div id=order_t_l>
-			订阅福布斯快闻　<input id="order_news" type="radio"><span style="font-size:12px; font-weight:normal; color:#666666;">我要定制</span>	
+			订阅福布斯快闻　<input id="order_news" type="radio"><span>我要定制</span>	
 		</div>
 		<div id=order_t_r>
 			<button disabled="disabled" id="news_order">定制</button>	
@@ -12,7 +12,7 @@
 	</div>
 	<div id=order_bot>
 		<div id=order_b_l>
-			<span style="font-size:14px; color:#11579e; font-weight:bold;">订阅分类文章</span>
+			<span>订阅分类文章</span>
 			<div class=cl><input class="order_article" name="rich" type="checkbox">富豪　<input class="order_article" name="create" type="checkbox">创业　<input class="order_article" name="business" type="checkbox">商业</div>
 			<div class=cl><input class="order_article" name="invest" type="checkbox">投资　<input class="order_article" name="life" type="checkbox">生活</div><input class="order_article" type="hidden" name="type" value="article">
 		</div>
