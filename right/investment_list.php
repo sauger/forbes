98066eb46b88f86676be67710a002efee3edfe5b
justin <zhanghao@xun-ao.com>
2010-04-23 12:24:24 +0800

    fix right

--- right/investment_list.php
+++ right/investment_list.php
@@ -2,12 +2,11 @@
 	<div id=wz><?php echo $catename[0]->name; ?>榜单</div>
 	<div id=more><a href=""><img border=0 src="/images/right/c_r_t_more.gif"></a></div>	
 </div>
-<div id=r_t_l></div>
-<div id=r_t_c>
+<div class="right_box">
 	<?php
 		for($i=0;$i<4;$i++){$pos_name = $pos."list".$i;
 	?>
 		<div class=content <?php show_page_pos($pos_name)?>><li><?php show_page_href($pos_items,$pos_name);?></li></div>
 	<?php } ?>
 </div>
-<div id=r_t_r></div>
+<div class="bottom_line"></div>
--- right/magazine.php
+++ right/magazine.php
@@ -1,31 +1,31 @@
 <div id="r_t_title">
 		<div id=wz>福布斯杂志</div>
 		<div id=more><a href="/magazine/"><img border=0 src="/images/right/c_r_t_more.gif"></a></div>	
-	</div>
-	<div id=mag_content <?php show_page_pos("right_magazine") ?>>
-			<div class=pic><?php show_page_img($pos_items,"right_magazine")?></div>
-			<div class=pictitle><?php show_page_href($pos_items,"right_magazine")?></div>
-			<div class=context><?php show_page_desc($pos_items,"right_magazine")?></div>	
- 			<div id=mag_dash></div>
-			<div id=search>往期杂志查阅</div>
-			<div id=sel>
-				<select id="old_magazine">
-					<option value=''></option>
-					<?php
-						$magazine = $db->query("select year from fb_old_magazine group by year order by year desc");
-						$year_count = $db->record_count;
-						for($i=0;$i<$year_count;$i++){
-					?>
-					<option value="<?php echo $magazine[$i]->year;?>"><?php echo $magazine[$i]->year;?>年</option>
-					<?php }?>
-				</select>
-				<select id="show_magazine">
-					<option value=""></option>
-				</select>
-			</div>
-			<a id="btnonline"></a>
-			<a id="sq"></a>
-			<div id=ck><a href="/magazine/list.php">查看杂志列表>></a></div>
+</div>
+<div id=mag_content <?php show_page_pos("right_magazine") ?>>
+		<div class=pic><?php show_page_img($pos_items,"right_magazine")?></div>
+		<div class=pictitle><?php show_page_href($pos_items,"right_magazine")?></div>
+		<div class=context><?php show_page_desc($pos_items,"right_magazine")?></div>	
+			<div id=mag_dash></div>
+		<div id=search>往期杂志查阅</div>
+		<div id=sel>
+			<select id="old_magazine">
+				<option value=''></option>
+				<?php
+					$magazine = $db->query("select year from fb_old_magazine group by year order by year desc");
+					$year_count = $db->record_count;
+					for($i=0;$i<$year_count;$i++){
+				?>
+				<option value="<?php echo $magazine[$i]->year;?>"><?php echo $magazine[$i]->year;?>年</option>
+				<?php }?>
+			</select>
+			<select id="show_magazine">
+				<option value=""></option>
+			</select>
+		</div>
+		<a id="btnonline"></a>
+		<a id="sq"></a>
+		<div id=ck><a href="/magazine/list.php">查看杂志列表>></a></div>
 
-	</div>
-	<div class=bottom_line></div>
+</div>
+<div class=bottom_line></div>
