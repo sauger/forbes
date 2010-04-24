101baedc147d36999fc83b475dd33ee61924b346
robbin <robbin@robbin-laptop.(none)>
2010-04-24 15:34:27 +0800

    robbin

---
+++ admin/seo/index.php
@@ -0,0 +1,61 @@
+<?php
+	session_start();
+	include_once('../../frame.php');
+	judge_role();
+	
+	$db = get_db();
+	$news = new table_class('fb_seo');
+	$record = $news->paginate('all','',30);
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<meta http-equiv=Content-Language content=zh-CN>
+	<title>发布seo</title>
+	<?php
+		css_include_tag('admin');
+		use_jquery();
+		js_include_tag('admin_pub','category_class');
+		$category->echo_jsdata();		
+	?>
+</head>
+<body>
+<div id=icaption>
+    <div id=title>seo管理</div>
+	  <a href="seo_edit.php" id=btn_add></a>
+</div>
+
+<div id=itable>
+	<table cellspacing="1" align="center">
+		<tr class=itable_title>
+			<td width="40%">标题</td><td width="20%">关键词</td><td width="20%">说明</td><td width="20%">操作</td>
+		</tr>
+		<?php
+			//--------------------
+			for($i=0;$i<count($record);$i++){
+		?>
+		<tr class=tr3 id=<?php echo $record[$i]->id;?> >
+			<td style="text-align:left; text-indent:12px;"><a href="<?php echo "/news/news.php?id={$record[$i]->id}";?>" target="_blank"><?php echo strip_tags($record[$i]->title);?></a></td>
+			<td><?php echo $record[$i]->author;?></td>
+			<td><a href="?category=<?php echo $record[$i]->category_id;?>" style="color:#0000FF"><?php echo $category->find($record[$i]->category_id)->name;?></a></td>
+			<td><?php echo $record[$i]->created_at;?></td>
+			<td>
+				<a href="news_edit.php?id=<?php echo $record[$i]->id;?>" class="edit" name="<?php echo $record[$i]->id;?>" title="编辑"><img src="/images/admin/btn_edit.png" border="0"></a>
+				<span style="cursor:pointer" class="del" name="<?php echo $record[$i]->id;?>"  title="删除"><img src="/images/admin/btn_delete.png" border="0"></span>
+			</td>
+		</tr>
+		<?php
+			}
+			//--------------------
+		?>
+		<tr class="btools">
+			<td colspan=10>
+				<?php paginate("",null,"page",true);?>
+			</td>
+		</tr>
+  </table>
+</div>	
+</body>
+</html>
