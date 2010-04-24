692d633ef8330f7b4137fe361dddd17535171d97
robbin <robbin@robbin-laptop.(none)>
2010-04-24 17:56:45 +0800

    robbin

--- admin/seo/seo_edit.php
+++ admin/seo/seo_edit.php
@@ -4,8 +4,8 @@
 	judge_role();
 	$id = $_REQUEST['id'];
 	if($id!='')	{
-		$video = new table_class('fb_seo');
-		$video->find($id);
+		$seo = new table_class('fb_seo');
+		$seo->find($id);
 	}
 ?>
 
@@ -29,16 +29,19 @@
 		<div id=itable>
 			<table cellspacing="1" align="center">
 				<tr class=tr4 align="center">
-					<td class=td1 width=15%>标　题</td><td align="left"><input id="title" type="text" name="seo[title]" value="<?php echo $video->title;?>"></td>
-				</tr>
-				<tr class=tr4 align="center">
-					<td class=td1>关键词</td><td align="left"><input type="text" size="50" name="seo[keywords]" value="<?php echo $video->keywords;?>">(请用空格或者","分隔开关键词,比如:高考 升学)</td>
+					<td class=td1 width=15%>名　称</td><td align="left"><input id="title" type="text" name="seo[name]" value="<?php echo $seo->name;?>"></td>
 				</tr>
 				<tr align="center" class=tr4>
-					<td class=td1>说　明</td><td align="left"><textarea name="seo[description]"><?php echo $video->description;?></textarea></td>
+					<td class=td1>标　题</td><td align="left"><input id="title" type="text" name="seo[title]" value="<?php echo $seo->title;?>" /></td>
+				</tr>
+				<tr class=tr4 align="center">
+					<td class=td1>关键词</td><td align="left"><input type="text" size="50" name="seo[keywords]" value="<?php echo $seo->keywords;?>">(请用空格或者","分隔开关键词,比如:高考 升学)</td>
+				</tr>
+				<tr align="center" class=tr4>
+					<td class=td1>说　明</td><td align="left"><textarea name="seo[description]"><?php echo $seo->description;?></textarea></td>
 				</tr>
 				<tr class=btools>
-					<td colspan="10" align="center"><input id="submit" type="submit" value="发布seo"></td>
+					<td colspan="10" align="center"><input id="submit" type="submit" value="发布"></td>
 				</tr>	
 			</table>
 			<input type="hidden" id="id" name="id" value="<?php echo $id;?>">
