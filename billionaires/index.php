0aa48419b57dfe8efe0f330e8ac23dbe8952dc8b
shelby <sunyoujie@xun-ao.com>
2010-04-24 15:57:13 +0800

    shelby1982

--- billionaires/index.php
+++ billionaires/index.php
@@ -1,5 +1,8 @@
 ﻿<?php 
 	require_once(dirname(__FILE__).'/../frame.php');
+	$db = get_db();
+	$nav=$db->query('select id from fb_navigation where name="富豪"');
+	$nav=$nav[0]->id;	
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
