74ad95a302120ca1b6bae3d4f193b6da553327c3
xiangxiangj <admin@.(none)>
2010-04-28 13:57:13 +0800

    xx

---
+++ search/index.php
@@ -0,0 +1,100 @@
+﻿<?php 
+	include_once('../frame.php');
+
+		$db = get_db();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<meta http-equiv=Content-Language content=zh-cn>
+	<title><?php echo strip_tags($news->short_title);?>-福布斯中文网</title>
+	<meta name="Keywords" content="<?php echo addslashes(strip_tags($news->keywords));?>"/>
+	<meta name="Description" content="<?php echo addslashes(strip_tags($news->keywords));?>"/>
+	<?php
+		use_jquery();
+		js_include_tag('public');
+		css_include_tag('public','x_index','right_inc','rich');
+	?>
+</head>
+<body>
+	<div id=ibody>
+		
+		<?php include "../inc/top.inc.php";?>
+		
+		<div id=bread>
+			
+				<span>关于我们</span>
+				
+		</div>
+		
+		<div id="hr_top"></div>
+		
+			<div id="left">
+					<div id="christcross">
+								<div id="christcross_left"></div>
+								<div id="christcross_middle">
+										<div id="chr_christcross">
+												<div id="chr_c_left">点击按字母排序：</div>
+												<div id="chr_c_right"><a href="#">A</a> <a href="#">B</a> <a href="#">C</a> <a href="#">D</a> <a href="#">E</a> <a href="#">F</a> <a href="#">G</a> <a href="#">H</a> <a href="#">I</a> <a href="#">J</a> <a href="#">K</a> <a href="#">L</a> <a href="#">M</a> <a href="#">N</a> <a href="#">O</a> <a href="#">P</a> <a href="#">Q</a> <a href="#">R</a> <a href="#">S</a> <a href="#">T</a> <a href="#">U</a> <a href="#">V</a> <a href="#">W</a> <a href="#">X</a> <a href="#">Y</a> <a href="#">Z</a></div>	
+										</div>
+										<div id="chr_sousuo">
+												按照投资行业索引：  <select name="select" style="width:240px; border:1px solid #B4B4BE;"></select>
+										<input type="button" name="str"  style="width:114px; height:37px; border:0px solid red; margin-left:60px; background:url(../images/search/button.gif) no-repeat;">
+										</div>							
+								
+								</div>
+								<div id="christcross_right"></div>
+						</div>
+					<div class=zh_left_c>
+						<div class=zh_left_c_left><img src="../images/search/1.jpg"></div>
+						<div class=zh_left_c_right>
+							<div  class=zh_left_name><strong>张默默</strong></div>
+									
+								<div class=zh_left_com>
+									<div  class=zh_left_CompanyName>北极光风险投资</div>
+									<div  class=zh_left_TypeofBusiness>合伙人</div>
+									</div>
+									<div class=zh_left_ii>
+									<div class=zh_left_investmentdirection>投资方向</div> 
+									<div  class=zh_left_investmentdirectioncontent>生物医药 电子商务 制造</div>
+									</div>
+							<div  class=zh_left_Newsofinvestment>投资动态</div>
+							<div class=zh_left_InvestmentVehicle>曹国伟酸雨不算精准把握；曹国伟酸雨不算精准把握</div>
+							<div class=zh_left_InvestmentVehicle>曹国伟酸雨不算精准把握；曹国伟酸雨不算精准把握</div>
+						</div>
+					</div>
+				<div class="c_hr"></div>
+				<div class=zh_left_c>
+						<div class=zh_left_c_left><img src="../images/search/1.jpg"></div>
+						<div class=zh_left_c_right>
+							<div  class=zh_left_name><strong>张默默</strong></div>
+									
+								<div class=zh_left_com>
+									<div  class=zh_left_CompanyName>北极光风险投资</div>
+									<div  class=zh_left_TypeofBusiness>合伙人</div>
+									</div>
+									<div class=zh_left_ii>
+									<div class=zh_left_investmentdirection>投资方向</div> 
+									<div  class=zh_left_investmentdirectioncontent>生物医药 电子商务 制造</div>
+									</div>
+							<div  class=zh_left_Newsofinvestment>投资动态</div>
+							<div class=zh_left_InvestmentVehicle>曹国伟酸雨不算精准把握；曹国伟酸雨不算精准把握</div>
+							<div class=zh_left_InvestmentVehicle>曹国伟酸雨不算精准把握；曹国伟酸雨不算精准把握</div>
+						</div>
+					</div>
+			</div>
+			
+		
+		<div id="right_inc">
+		 		<?php include "../right/ad.php";?>
+		 		<?php include "../right/favor.php";?>
+		 		<?php include "../right/four.php";?>
+		 		<?php include "../right/rich.php";?>
+		 		<?php include "../right/magazine.php";?>
+		</div>
+		<?php include "../inc/bottom.inc.php";?>
+		
+	</div>
+</body>
+<html>
---
+++ survey/list.php
@@ -0,0 +1,48 @@
+﻿<?php 
+	include_once('../frame.php');
+	$db = get_db();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+	<meta http-equiv=Content-Language content=zh-cn>
+	<title>福布斯-新闻列表</title>
+	<?php
+		use_jquery();
+		js_include_tag('public','right');
+		css_include_tag('news','public','right_inc','x_list');
+	?>
+</head>
+<body <?php if($news->forbbide_copy == 1){ ?> oncontextmenu="return false" ondragstart="return false" onselectstart ="return false" onselect="return false" oncopy="return false" onbeforecopy="return false" onmouseup="return false" <?php }?>>
+<div id=ibody>
+		<?php include "../inc/top.inc.php";?>
+		<div id=bread>			
+				<span>问卷调查</span>				
+		</div>
+		<div id="hr_top"></div>
+		
+		<div id="left_content">
+			<div id="title"><div id="title_a"></div><div id="title_b">问卷调查</div> <div id="title_num">共18篇</div></div>
+			<div id="content">
+					<div id="c_c"><div class="c_left_t">标题：</div>
+							<div id="c_title">“重启”瘫痪肢体</div><div id="author">《福布斯》　记者：Jonathan Fahey</div><div id="time">回复于：2010-04-16</div>
+					</div>
+					<div id="c_bottom">医疗专家们正在研究如何重建神经系统，以便使何重建神经系统，以便使何重建神经系统，以便使何重建神经系统，以便使瘫痪的肢体重现活力。</div>
+			</div>
+			<div class="hr"></div>
+			
+		</div>
+		
+		
+		<div id="right_inc">
+			<?php include "../right/ad.php";?>
+			<?php include "../right/favor.php";?>
+			<?php include "../right/four.php";?>
+			<?php include "../right/forum.php";?>
+			<?php include "../right/magazine.php";?>
+		</div>
+		<?php include "../inc/bottom.inc.php";?>
+</div>
+</body>
+<html>
