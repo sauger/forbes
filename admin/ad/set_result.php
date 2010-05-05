1d0e62bee91a30312ee8836efb0e7f476e1ebd88
justin <zhanghao@xun-ao.com>
2010-05-05 20:09:00 +0800

    fix ad

--- admin/ad/set_result.php
+++ admin/ad/set_result.php
@@ -2,6 +2,11 @@
     include_once('../../frame.php');
 	
 	$db = get_db();
+	$date = date("Y-m-d");
 	
-	
+	$record = $db->query("select count(id) from forbes_ad.fb_ad_show_list where substring(created_at,1,10)='$date' group by ad_id");
+	$count = $db->record_count;
+	for($i=0;$i<$ount;$i++){
+		
+	}
 ?>
