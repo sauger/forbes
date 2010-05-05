70c41f418dc13d35d6f5f3d7a00fbe907219f370
justin <zhanghao@xun-ao.com>
2010-05-05 19:41:40 +0800

    fix ad

---
+++ dbscript/create_ad_result.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE `forbes_ad`.`fb_ad_result` (
+  `id` INTEGER UNSIGNED NOT NULL,
+  `source_id` INTEGER UNSIGNED,
+  `type` VARCHAR(20),
+  `date_time` DATETIME,
+  `count` INTEGER UNSIGNED DEFAULT 0,
+  PRIMARY KEY (`id`)
+)
