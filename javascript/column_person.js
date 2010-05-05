6e5a7bdfc7ad6d3fc161b606ba56196189ffc991
robbin <robbin@robbin-laptop.(none)>
2010-05-05 11:27:27 +0800

    robbin

--- javascript/column_person.js
+++ javascript/column_person.js
@@ -1,15 +1,12 @@
 $(function(){
 	$('.other_title').click(function(){
-		for(var i=1;i<4;i++)
+		var i;
+		for(i=1;i<4;i++)
 		{
 			$('#othertitle'+i).attr('class','other_title');
 		}
 		
-		$(this).attr('class','dq_title');
-		if($(this).attr('param')!="1")
-		{	
-			$(this).css('margin-left','60px;');		
-		}
+		$(this).attr('class','dq_title');	
 		var iframsrc='iframe.php?id='+$('#columnid').attr('value')+'&type='+$(this).attr('param1')+'&date='+$('#columndate').attr('value');
 		$('#iframesrc').attr('src',iframsrc);
 	});
