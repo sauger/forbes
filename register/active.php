ba8ae82c984e4d50ef38bca71603c51ead8a8b26
justin <zhanghao@xun-ao.com>
2010-05-06 18:50:54 +0800

    fix active

--- register/active.php
+++ register/active.php
@@ -1,28 +1,36 @@
+<?php
+	include '../frame.php';
+	use_jquery();
+	$user = $_GET['user'];
+	$key = $_GET['key'];
+	$db = get_db();
+	$db->query("select name,id,password from fb_yh where name='{$user}' and authenticate_string='{$key}'");
+	
+	if(!$db->move_first()){
+		$str = '对不起，您的验证码不正确，无法完成激活！';
+	}else{
+		$id = $db->field_by_name('id');
+		$name = $db->field_by_name('name');
+		$user_id = $db->field_by_name('id');
+		$password = $db->field_by_name('password');
+		$db->execute("update fb_yh set authenticated=1 where id={$id}");
+		adjust_user_score($id,50,"用户激活");
+		$str = '恭喜您，激活成功，感谢您注册成为福布斯中文网会员！';
+		$cache_name = sprintf('%06s',$user_id) .rand_str(24);
+		$limit=3600*24;
+		setcookie("name",$name,time()+$limit,'/');
+		setcookie("cache_name",$cache_name,time()+$limit,'/');
+		setcookie("password",$password,time()+$limit,'/');
+	}
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<meta http-equiv=Content-Language content=zh-cn>
 	<title>福布斯中文网-用户激活</title>
-	<?php
-		include '../frame.php';
-		use_jquery();
-		$user = $_GET['user'];
-		$key = $_GET['key'];
-		$db = get_db();
-		$db->query("select name,id from fb_yh where name='{$user}' and authenticate_string='{$key}'");
-		
-		if(!$db->move_first()){
-			alert('对不起，您的验证码不正确，无法完成激活！');
-			redirect('/');
-			die();
-		}else{
-			$id = $db->field_by_name('id');
-			$db->execute("update fb_yh set authenticated=1 where id={$id}");
-			adjust_user_score($id,50,"用户激活");
-			alert('恭喜您，激活成功，感谢您注册成为福布斯中文网会员！');
-			redirect('/login/');
-		}
-	?>
 </head>
 <?php
+alert($str);
+redirect('/');
+?>
