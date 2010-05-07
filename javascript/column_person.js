0e395341cd90587eea3dde249fc76e3c8be60226
sauger <sauger@163.com>
2010-05-07 19:59:59 +0800

    fix bugs

--- javascript/column_person.js
+++ javascript/column_person.js
@@ -1,13 +1,24 @@
 $(function(){
 	$('.other_title').click(function(){
-		var i;
-		for(i=1;i<4;i++)
-		{
-			$('#othertitle'+i).attr('class','other_title');
-		}
-		
-		$(this).attr('class','dq_title');	
-		var iframsrc='iframe.php?id='+$('#columnid').attr('value')+'&type='+$(this).attr('param1')+'&date='+$('#columndate').attr('value');
+		$('.other_title').removeClass('selected');
+		$(this).addClass('selected');	
+		//alert($('#columnid').attr('value'));
+		var iframsrc='iframe.php?id='+$('#columnid').attr('value')+'&type='+$(this).attr('id')+'&date='+$('#columndate').attr('value');
 		$('#iframesrc').attr('src',iframsrc);
+	});
+	$('#btn_collect').click(function(){
+		$.getScript('/ajax/collect_column.php?column_id=' + $('#columnid').val());
+	});
+	$('.c_b_content a').click(function(e){
+		e.preventDefault();
+		if($(this).hasClass('selected')){
+			$('#columndate').val('');
+			$(this).removeClass('selected');
+		}else{
+			$('#columndate').val($(this).html());
+			$('.c_b_content a').removeClass('selected');
+			$(this).addClass('selected');
+		}
+		$('.other_title.selected').click();
 	});
 });
