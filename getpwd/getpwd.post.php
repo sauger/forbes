45aadc1946a7be09f0dc4ec0ae3cef3201e5d947
justin <zhanghao@xun-ao.com>
2010-05-07 11:32:58 +0800

    push

--- getpwd/getpwd.post.php
+++ getpwd/getpwd.post.php
@@ -37,8 +37,8 @@
 		$gp->verify = $verify;
 		$gp->end_time = dt_increase(4,'h');
 		$gp->save();
-		$content = "欢迎进行福布斯中文网密码重置过程，请点击下面的链接：<br/><a href='http://www.forbeschina.com/getpwd/get_pwd.php?verify=$verify'>http://www.forbeschina.com/getpwd/get_pwd.php?verify=$verify</a><br>如果点击以上链接不起作用，请将此网址复制并粘贴到新的浏览器窗口中。如果您意外地收到此邮件，很可能是其他用户在尝试重设密码时，误输入了您的电子邮件地址。如果您没有提出此请求，则无需做进一步的操作，可以放心地忽略此电子邮件。";
-		send_mail('smtp.qiye.163.com','userservice@forbeschina.com','userservice','userservice@forbeschina.com',$mail,'福布斯中文网',$content);
+		$content = "{$name},你好：<br/><br/>　　欢迎进行福布斯中文网密码重置过程，请点击下面的链接：<br/>　　<a href='http://www.forbeschina.com/getpwd/get_pwd.php?verify=$verify'>http://www.forbeschina.com/getpwd/get_pwd.php?verify=$verify</a><br><br>　　如果点击以上链接不起作用，请将此网址复制并粘贴到新的浏览器窗口中。如果您意外地收到此邮件，很可能是其他用户在尝试重设密码时，误输入了您的电子邮件地址。如果您没有提出此请求，则无需做进一步的操作，可以放心地忽略此电子邮件。";
+		send_mail('smtp.qiye.163.com','userservice@forbeschina.com','userservice','userservice@forbeschina.com',$mail,'福布斯中文网(Forbeschina.com)密码找回',$content);
 		alert("请尽快登录到".$mail."完成剩余操作！");
 		redirect('/');
 	}else{
