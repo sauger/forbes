f7fc0290b8de06dfca299f84444853150109fea2
robbin <robbin@robbin-laptop.(none)>
2010-05-07 11:13:59 +0800

    robbin

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
 								<?php for($j=1;$j<5;$j++){
 									$field = "field_{$j}";
 								?>
-								<td style="color:<?php if($j==2){ echo "#1649A2";} ?>" align="center" nowrap><?php echo $items[$i]->$field;?></td>
+								<td style="color:<?php if($j==2){ echo "#1649A2";}else if($j==3){echo "#000000";} ?>" align="center" nowrap><?php echo $items[$i]->$field;?></td>
 								<?php }?>
 							</tr>						
 						<?php }?>
