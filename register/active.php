084dcc7b571b6ce47f611cb1cffab6766019a0b1
justin <zhanghao@xun-ao.com>
2010-05-08 17:23:32 +0800

    survey

--- register/active.php
+++ register/active.php
@@ -8,21 +8,19 @@
 	if(!$db->move_first()){
 		$str = '对不起，您的验证码不正确，无法完成激活！';
 	}else{
-		$id = $db->field_by_name('id');
 		$name = $db->field_by_name('name');
 		$user_id = $db->field_by_name('id');
 		$password = $db->field_by_name('password');
 		$authenticated = $db->field_by_name('authenticated');
 		if($authenticated==0){
-			$db->execute("update fb_yh set authenticated=1 where id={$id}");
+			$db->execute("update fb_yh set authenticated=1 where id={$user_id}");
 			adjust_user_score($id,50,"用户激活");
 		}
 		$str = '恭喜您，激活成功，感谢您注册成为福布斯中文网会员！';
 		$cache_name = sprintf('%06s',$user_id) .rand_str(24);
-		$limit=3600*24;
-		setcookie("name",$name,time()+$limit,'/');
-		setcookie("cache_name",$cache_name,time()+$limit,'/');
-		setcookie("password",$password,time()+$limit,'/');
+		setcookie("name",$name,0,'/');
+		setcookie("cache_name",$cache_name,0,'/');
+		setcookie("password",$password,0,'/');
 	}
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
