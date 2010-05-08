21ddfb6f0120585ec3256748c78dc55744d9fcc1
justin <zhanghao@xun-ao.com>
2010-05-08 16:58:53 +0800

    static_news

--- admin/tools/static_all_news.php
+++ admin/tools/static_all_news.php
@@ -1,7 +1,20 @@
 <?php
-include "../../frame.php";
+session_start();
+include_once('../../frame.php');
+judge_role();
+
+$start = $_POST['start'];
+$end = $_POST['end'];
+$sql = "select id from fb_news where is_adopt=1 and language_tag = 0";
+if($start!=''){
+	$sql .= " and created_at>'{$start}'";
+}
+if($end!=''){
+	$sql .= " and created_at<'{$end}'";
+}
 $db = get_db();
-$news_id = $db->query("select id from fb_news where is_adopt=1 and language_tag = 0");
+$news_id = $db->query($sql);
+echo '静态条数：'.$db->record_count;die();
 if($db->record_count > 0 ){
 	$len = $db->record_count;
 	$news = new table_class('fb_news');
