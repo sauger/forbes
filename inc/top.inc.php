8d8d9c389f5f71f33b5af5afd8bcd2820c67b252
justin <zhanghao@xun-ao.com>
2010-05-08 16:22:51 +0800

    top fix

--- inc/top.inc.php
+++ inc/top.inc.php
@@ -6,7 +6,7 @@
 		init_page_items();
 ?>
 	<div id=top_>
-		<div id=top_banner class="ad_banner">\
+		<div id=top_banner class="ad_banner">
 		<!--
 	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" width="778" height="90">
         <param name="movie" value="/flash/banner.swf">
