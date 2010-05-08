10d2fdf7ab779c8be0531c71df5894625969e5fc
robbin <robbin@robbin-laptop.(none)>
2010-05-08 17:18:50 +0800

    robbin

--- index.php
+++ index.php
@@ -370,10 +370,10 @@
 		
 		<div class=forbes_r>
 			<div id=dictionary>
-				<div id=dictionary_l <?php $pos_name ="dictionary_l_content1"; show_page_pos($pos_name);?>><?php show_page_href()?></div>
+				<div id=dictionary_l <?php $pos_name ="dictionary_l_content1"; show_page_pos($pos_name,'dictionary');?>><?php show_page_href()?></div>
 				<div id=dictionary_r>
-					<div class=content <?php $pos_name ="dictionary_r_content1"; show_page_pos($pos_name);?>><?php show_page_href()?></div>
-					<div class=content <?php $pos_name ="dictionary_r_content2"; show_page_pos($pos_name);?>><?php show_page_href()?></div>
+					<div class=content <?php $pos_name ="dictionary_r_content1"; show_page_pos($pos_name,'dictionary');?>><?php show_page_href()?></div>
+					<div class=content <?php $pos_name ="dictionary_r_content2"; show_page_pos($pos_name,'dictionary');?>><?php show_page_href()?></div>
 					<div class=content></div>
 				</div>
 			</div>
