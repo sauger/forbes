3bf327035359fac59c6736afc5636558db724743
sauger <sauger@163.com>
2010-05-09 19:35:28 +0800

    fix bugs

--- ajax/comment.php
+++ ajax/comment.php
@@ -1,10 +1,10 @@
 <?php
 	include_once('../frame.php');
 	
-	if(is_numeric($_POST['news_id'])===false) die();
 	$db = get_db();
 	
 	if($_POST['type']=='comment'){
+		if(is_numeric($_POST['news_id'])===false) die();
 		$comment = new table_class('fb_comment');
 		$comment->resource_type = "news";
 		$comment->resource_id = intval($_POST['news_id']);
@@ -18,8 +18,10 @@
 		$comment->save();
 		echo $comment->id;
 	}else if($_POST['type']=='up'){
+		if(is_numeric($_POST['id'])===false) die();
 		$db->execute("insert into fb_comment_dig (comment_id,up) values ({$_POST['id']},1) ON DUPLICATE KEY update up=up+1");
 	}else if($_POST['type']=='down'){
+		if(is_numeric($_POST['id'])===false) die();
 		$db->execute("insert into fb_comment_dig (comment_id,down) values ({$_POST['id']},1) ON DUPLICATE KEY update down=down+1");
 	}	
 ?>
