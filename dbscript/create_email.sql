cb88e309834f42dee4e0258bb9afcef77d85f6a8
justin <zhanghao@xun-ao.com>
2010-05-09 16:20:00 +0800

    add email db

---
+++ dbscript/create_email.sql
@@ -0,0 +1,10 @@
+﻿CREATE DATABASE IF NOT EXISTS forbes_email;
+CREATE TABLE `forbes_email`.`fb_email` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `to` VARCHAR(256),
+  `from` VARCHAR(256),
+  `subject` VARCHAR(256),
+  `content` TEXT,
+  `status` INTEGER UNSIGNED,
+  PRIMARY KEY (`id`)
+)
