60dea510a7f3cbcfafe008b23c623261500c0f36
sauger <sauger@163.com>
2010-05-11 14:18:03 +0800

    fix bugs

--- column/column_img.php
+++ column/column_img.php
@@ -1,4 +1,5 @@
 <?php include_once(dirname(__FILE__).'/../frame.php');
+	if(!is_numeric($_REQUEST['id'])) die();
 	$db=get_db();
 	$img=$db->query('select src from fb_images where id='.$_REQUEST['id']);
 	echo '<img width=754 src="'.$img[0]->src.'" />';
