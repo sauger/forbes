1f98e7398481c12e01768e8338b70cc20151f24d
sauger <sauger@163.com>
2010-05-11 16:56:30 +0800

    fix bugs

--- javascript/get_ad.js
+++ javascript/get_ad.js
@@ -12,7 +12,6 @@ $(function(){
 		$(this).load('/ajax/load_ad.php?channel='+channel+ '&banner='+$(this).attr('id'),{url:location.pathname});
 	});
 	$('div.ad_banner').live('click',function(){
-		alert($(this).find('input:first').val());
 		$.post('/ajax/add_click_ad.php',{'code':$(this).find('input:first').val(),url:location.pathname});
 	});
 	
