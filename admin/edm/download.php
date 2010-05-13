dada0b8a1a28ac74c1476d6100a42cee3746ca86
justin <zhanghao@xun-ao.com>
2010-05-13 17:37:37 +0800

    test email

--- admin/edm/download.php
+++ admin/edm/download.php
@@ -2,7 +2,7 @@
 include "../../frame.php";
 $str = file_get_contents("http://localhost/admin/edm/edm.php?page_type=static");
 
-send_mail('smtp.qiye.163.com','userservice@forbeschina.com','userservice','userservice@forbeschina.com','411192132@163.com','福布斯中文网(Forbeschina.com)注册确认邮件',$str);
+send_mail('smtp.qiye.163.com','userservice@forbeschina.com','userservice','userservice@forbeschina.com','411192132@163.com','EDM',$str);
 die();
 Header("Content-type: application/octet-stream"); 
 
