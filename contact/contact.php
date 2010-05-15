a3d195a560baaed7f9070db46e25ed7291c5b1e4
shelby <sunyoujie@xun-ao.com>
2010-05-15 18:06:17 +0800

    contact

--- contact/contact.php
+++ contact/contact.php
@@ -65,7 +65,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title><?php echo $title;?>_福布斯中文网</title>
+	<title><?php echo $a[$name];;?>_福布斯中文网</title>
 	<meta http-equiv=Content-Type content="text/html; charset=utf-8">
 	<meta http-equiv=Content-Language content=zh-cn>
 	<?php
