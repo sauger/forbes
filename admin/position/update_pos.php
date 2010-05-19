3b7928ee4534438e2ee461a89ef4e43914577234
sauger <sauger@163.com>
2010-05-19 18:07:16 +0800

    fix bugs

--- admin/position/update_pos.php
+++ admin/position/update_pos.php
@@ -212,7 +212,7 @@ function update_column2($type,$limit,$position_name,$news_limit,$news_position,$
 	
 	for($k=0;$k<$limit;$k++){
 		$pos_name = $position_name.$k;
-		$column = $db->query("select t2.id,t2.name from fb_page_pos t1 join fb_user t2 on t1.alias=t2.name where t1.name='{$pos_name}' and t2.role_name='{$type}'");
+		$column = $db->query("select t2.id,t2.name from fb_page_pos t1 join fb_user t2 on t1.alias=t2.name where t1.name='{$pos_name}' ");
 		if($db->record_count==1){
 			$sql = "select distinct(title) as title,id,short_title,created_at,description,video_photo_src from fb_news where publisher={$column[0]->id} order by created_at desc limit {$news_limit}";
 			$news = $db->query($sql);
@@ -221,7 +221,7 @@ function update_column2($type,$limit,$position_name,$news_limit,$news_position,$
 			$news_count = 0;
 		}
 		
-		for($i=0;$i<$news_count;$i++){
+		for($i=0;$i<4;$i++){
 			$pos_name = $position_name.$k.$news_position.$i;
 				$record = $db->query("select id,end_time from fb_page_pos where name='{$pos_name}'");
 				if($db->record_count==1){
