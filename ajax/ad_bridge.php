877db24d80f4b3f7b4f85b72a595a6afc5dc8a73
sauger <sauger@163.com>
2010-05-27 13:08:15 +0800

    fix bugs

--- ajax/ad_bridge.php
+++ ajax/ad_bridge.php
@@ -3,12 +3,12 @@ include_once "../frame.php";
 $ref = $_SERVER['HTTP_REFERER']; 
 if(strpos($ref,$site_domain) !== 0){
 	//die(1);
-	//die_error();
+	die_error();
 }
 $target_url = $_GET['target_url'];
 $code = $_GET['code'];
 $db= get_db();
 $ad = $db->query("select * from forbes_ad.fb_ad where id = '{$_GET['code']}'");
-if(empty($ad)) die(2);
+if(empty($ad)) die_error();
 insert_ad_record($ad[0],'click');
 redirect($ad[0]->target_url ? $ad[0]->target_url : $site_domain,'header');
