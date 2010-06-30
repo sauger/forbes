80ea5928542862e34e9e321810ccb14667b052b3
justin <zhanghao@xun-ao.com>
2010-06-30 15:04:23 +0800

    fix pos

--- admin/position/_index.php
+++ admin/position/_index.php
@@ -1,7 +1,7 @@
 <?php
-#update_pos("创业",4,'index_bus',true);
-#update_pos("商业",5,"index_business",true);
-update_pos("科技",5,"index_tech",true);die();
+update_pos("创业",4,'index_bus',true);
+update_pos("商业",5,"index_business",true);
+update_pos("科技",5,"index_tech",true);
 update_pos("投资",6,"index_invest",true);
 update_click(6,'index_pop');
 #update_column('column_editor',8,'index_jour');
