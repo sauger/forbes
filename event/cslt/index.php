a7bd150a03792013394465e343dcb1e0bb13ddf3
justin <zhanghao@xun-ao.com>
2010-08-12 17:42:16 +0800

    cslt

---
+++ event/cslt/index.php
@@ -0,0 +1,207 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>FBS</title>
+<script type="text/javascript" language="javascript" src="./javascript/mini.js"></script>
+<script type="text/javascript" language="javascript" src="./javascript/fbs.js"></script>
+<link href="./css/fbs.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+
+<div id="ibody">
+<div id="part1"></div>
+<div id="part2">
+	<div id="p2_navbox">
+		<div id="nav_0" class="nav_menu">首页</div>
+		<div class="hinder"></div>
+		<div id="nav_1" class="nav_menu">常熟介绍</div>
+		<div class="hinder"></div>
+		<div id="nav_2" class="nav_menu">论坛背景</div>
+		<div class="hinder"></div>
+		<div id="nav_3" class="nav_menu">论坛介绍</div>
+		<div class="hinder"></div>
+		<div id="nav_4" class="nav_menu">媒体合作</div>
+		<div class="hinder"></div>
+		<div id="nav_5" class="nav_menu">拟要嘉宾</div>
+		<div class="hinder"></div>
+		<div id="nav_6" class="nav_menu">组织构架</div>
+	</div>
+</div>
+<div id="part3">
+	<div id="p_left">
+		<div class="framwork">
+			<div class="fram_title" style="width:223px; border:0;">
+				组织构架
+			</div>
+			<div class="fram_line">
+				<div class="line_left"></div>
+				<div class="line_right"></div>
+			</div>
+			<div class="fram_content">
+				<div class="c_line">
+					<div class="c_name">主办单位：</div>
+					<span>&nbsp;&nbsp;《福布斯》中文版</span>
+				</div>
+				<div class="c_line">
+					<div class="c_name">联合主办：</div>
+					<span>&nbsp;&nbsp;常熟市人民政府</span>
+				</div>
+				<div class="c_line">
+					<div class="c_name">协办单位：</div>
+					<span>&nbsp;&nbsp;波司登国际控股有限公司</span>
+				</div>
+				<div class="c_line">
+					<span>江苏常熟农村商业银行股份有限公司</span>
+				</div>
+				<div class="c_line">
+					<span>江苏隆力奇生物科技股份有限公司</span>
+				</div>
+				<div class="c_line" style="height:24px">
+					<div class="c_name">活动时间：</div>
+					<div id="c_time">2010年9月17日8:30 - 17:30</div>
+				</div>
+				<div class="c_line">
+					<div class="c_name">活动地点：</div>
+					<span>&nbsp;&nbsp;江苏&nbsp;常熟</span>
+				</div>
+				<div class="c_line">
+					<div class="c_name">活动规模：</div>
+					<span>&nbsp;&nbsp;200人</span>
+				</div>
+			</div>
+			<div class="fram_filing" id="no1"></div>
+		</div>
+		<div class="framwork">
+			<div class="fram_title">
+				<font>合作媒体</font>
+			</div>
+			<div class="fram_line">
+				<div class="line_left"></div>
+				<div class="line_right"></div>
+			</div>
+			<div class="fram_content">
+				<div id="logobox">
+					<div id="logo_sina">独家门户网站支持
+						<a href="#"><img src="./images/logo_sina.jpg" border=0 /></a>
+					</div>
+					<div id="logo_msn">特别媒体支持
+						<a href="#"><img src="./images/logo_msn.jpg" border=0 /></a>
+					</div>
+					<div id="logo_JRJ">特邀财经网站支持：
+						<a href="#"><img src="./images/logo_JRJ.jpg" border=0 /></a>
+					</div>
+				</div>
+					<div class="c_name" style="font-size:12px; line-height:12px; margin-top:20px; margin-left:14px;">战略媒体支持：</div>
+				<div class="logo_box">
+					<div id="logo_21">
+						<a href="#"><img style="margin-left:29px;"src="./images/logo_21.jpg" border=0 /></a>
+					</div>
+					<div id="logo_weekly">
+						<a href="#"><img style="margin-top:8px;"src="./images/logo_weekly.jpg " border=0 /></a>
+					</div>
+					<div id="logo_sound" style="margin-left:10px;">
+						<a href="#"><img style="margin-top:8px;"src="./images/logo_sound.jpg" border=0 /></a>
+					</div>
+				</div>
+					<div class="c_name" style="font-size:12px; line-height:12px; margin-top:19px; margin-left:14px;">友情支持：</div>
+				<div class="logo_box">
+					<div id="logo_ce">
+						<a href="#"><img src="./images/logo_ce.jpg " border=0 /></a>
+					</div>
+					<div id="logo_qj" style="margin-left:5px;">
+						<a href="#"><img src="./images/logo_qj.jpg " border=0 /></a>
+					</div>
+				</div>
+			</div>
+			<div class="fram_filing" id="no2"></div>
+			</div>
+		<div class="framwork">
+			<div class="fram_title">
+			<font>参会说明</font>
+			</div>
+			<div class="fram_line">
+				<div class="line_left"></div>
+				<div class="line_right"></div>
+			</div>
+			<div class="fram_content">
+				<div id="f_text">　　县域经济是国民经济的重要组成部分，也是国民经济的基本单元，只有县域经济得到充分发展，才能从根</div>
+				<div id="down_apply">
+					<a href=""><img src="./images/down_apply.jpg" border=0 /></a>
+				</div>
+			</div>
+			<div class="fram_filing" id="no3"></div>
+			</div>
+			<div class="framwork">
+			<div class="fram_title">
+				<font>活动信息</font>
+			</div>
+			<div class="fram_line">
+				<div class="line_left"></div>
+				<div class="line_right"></div>
+			</div>
+			<div class="fram_content" style="height:260px;">
+			</div>
+		</div>
+	</div>
+	<div id="p_middle">
+		<div class="photo_box">
+			<div id="m_photo">
+				<div id="m_title">图片快照</div>
+				<div id="m_tl">SNAPSHOT</div>
+			</div>
+			<div id="p4_box">
+				<div id="p4_left">
+					<a href=""><img src="./images/fengye.jpg" border=0 /></a>
+					<div class="pho_name">枫叶</div>	
+					<a href=""><img src="./images/fengye.jpg" border=0 /></a>
+					<div class="pho_name">枫叶</div>	
+				</div>
+				<div id="p4_right">
+					<a href=""><img src="./images/luweidang.jpg" border=0 /></a>
+					<div class="pho_name">芦苇荡</div>	
+					<a href=""><img src="./images/luweidang.jpg" border=0 /></a>
+					<div class="pho_name">芦苇荡</div>	
+				</div>
+			</div>
+		</div>
+		<div class="photo_box" style="margin-top:20px">
+			<div id="m_photo">
+				<div id="m_title">常熟介绍</div>
+				<div id="m_tl">SNAPSHOT</div>
+			</div>
+			<div id="m_textbox">
+				<div id="m_text">
+					<span><a href=""><img style="margin-top:6px; float:left;"src="./images/luweidang.jpg" border=0/></a>　　常熟位于“长三角”经济带中心，市域面积1264平方公里，常住人口106万，综合实力一直位居全国百强县（市）前列，是江苏省首批“全面小康”县（市）。
+常熟是一座历史底蕴深厚的文化之城。常熟有着3000年文明史、1700年建城史，是江苏县（市）中唯一的国家历史文化名城。自唐至清，共出了8名状元、9名宰相、485名进士，诗文、琴棋、书画、金石、戏曲等文化艺术领域名家辈出。当代常熟籍“两院”院士有21人。
+常熟是一座生态环境优美的山水之城。常熟古城三湖环抱，虞山尚湖相映成辉，沙家浜名闻遐迩，自然风光秀丽。近年来，常熟加强生态建设，优化人居环境，先后获得全国同类城市中首家“国际花园城市”、“国家园林城市”和全国首批“国家生态市”等荣誉。</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="p_right">
+		<div id="r_box">
+			<div id="r_photo">
+				<div id="r_title">背景介绍</div>
+				<div id="r_tl">BACKGROUND</div>
+			</div>
+			<div id="r_textbox">
+				<div id="r_text">县域经济是国民经济的重要组成部分，也是国民经济的基本单元，只有县域经济得到充分发展，才能从根本上推动和加快现代化建设和全面建设小康社会的进程。中央经济工作会议明确指出，要发挥好城市的辐射带动作用，把发展重点放在加强中小城市和小城镇发展上。
+近几年来，随着国企改制，城乡发展迅速壮大，县级城市在促进城乡统筹发展以及东西部经济繁荣上做出了突出的工作。2009年，县域经济总量已占全国的56.31%，地方财政收入占全国的1/4。长三角地区一体化趋势显著，以江苏为代表的县域经济，在集体经济改制激发企业发展活力的基础上，抓住对外开放的机遇，走出了一条外向型经济带动县域经济发展的道路。但是，县域经济大力</div>
+			</div>
+		</div>
+		<div id="r_box" style="margin-top:20px;">
+			<div id="r_photo">
+				<div id="r_title">背景介绍</div>
+				<div id="r_tl">BACKGROUND</div>
+			</div>
+			<div id="r_textbox">
+				<div id="r_text"></div>
+			</div>
+		</div>
+		</div>
+</div>
+</div>
+	
+</body>
+</html>
---
+++ event/cslt/javascript/fbs.js
@@ -0,0 +1,6 @@
+$(function(){
+	$('.nav_menu').hover(function(){
+		$('.nav_menu').css('background','none');
+		$(this).css('background','url("./images/nav_hover_bj.jpg") repeat-x');
+	});
+});
