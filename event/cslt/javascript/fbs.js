c68107bc14c6aa088f66fdd90151c06e9817b49a
justin <zhanghao@xun-ao.com>
2010-08-17 15:26:55 +0800

    cslt

--- event/cslt/javascript/fbs.js
+++ event/cslt/javascript/fbs.js
@@ -24,7 +24,7 @@ $(function(){
 				window.location.href="./test_media.html";
 			}
 		else if(selected === 6){
-			window.location.href="./test_past.html";
+			window.location.href="./test_pasttime.html";
 			}
 	});
 });
