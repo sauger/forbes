90567ea3ccb621280b408c6a4f5e6763b59b2d80
justin <zhanghao@xun-ao.com>
2010-08-18 14:01:47 +0800

    index

--- index_two.php
+++ index_two.php
@@ -80,8 +80,8 @@
 						<div class="guide_title">基金经理看市</div>
 						<div class="guide_hr"></div>
 						<a href="/review/list/145"><img class="guide_more" src="/images/index_two/g4.jpg"/></a>
-						<div class="gg_banner" <?php show_page_pos($pos_name,'base_img')?>>
-							<div class="g_pg"><?php show_page_img();?></div>
+						<div class="gg_banner">
+							<div class="g_pg" <?php show_page_pos($pos_name,'base_img')?>><?php show_page_img();?></div>
 							<div class="g_pg_title"><?php show_page_href();?></div>
 							<div class="g_pg_value"><?php show_page_desc();?></div>
 						</div>
@@ -91,7 +91,7 @@
 						<?php for($i=1;$i<3;$i++){$pos_name = "index_zczx_".$i;?>
 						<div class="guide_hr_val" <?php show_page_pos($pos_name,'link_withouttime')?>><?php show_page_href();?></div>
 						<?php }?>
-						<div class="gg_title" style="margin-top:10px;">投票之选</div>
+						<div class="gg_title" style="margin-top:10px;">股票之选</div>
 						<div class="guide_hr" style="width:150px; margin-top:15px;"></div>
 						<a href="/review/list/147"><img class="guide_more" src="/images/index_two/g4.jpg"  style="margin-top:10px;"/></a>
 						<?php for($i=1;$i<3;$i++){$pos_name = "index_tpzx_".$i;?>
@@ -106,8 +106,8 @@
 					</div>
 					<div class="guide" id="jhdd">
 						<?php $pos_name = 'index_jhdd_0';?>
-						<div class="gg_banner" style="height:90px;" <?php show_page_pos($pos_name,'base_img')?>>
-							<div class="g_pg"><?php show_page_img();?></div>
+						<div class="gg_banner" style="height:90px;">
+							<div class="g_pg" <?php show_page_pos($pos_name,'base_img')?>><?php show_page_img();?></div>
 							<div class="g_pg_title"><?php show_page_href();?></div>
 							<div class="g_pg_value"><?php show_page_desc();?></div>
 						</div>
@@ -116,8 +116,8 @@
 						<?php }?>
 						<div class="guide_hr" style="width:240px; margin-top:15px;"></div>
 						<?php $pos_name = 'index_jhdd_4';?>
-						<div class="gg_banner" style="height:90px;" <?php show_page_pos($pos_name,'base_img')?>>
-							<div class="g_pg"><?php show_page_img();?></div>
+						<div class="gg_banner" style="height:90px;">
+							<div class="g_pg" <?php show_page_pos($pos_name,'base_img')?>><?php show_page_img();?></div>
 							<div class="g_pg_title"><?php show_page_href();?></div>
 							<div class="g_pg_value"><?php show_page_desc();?></div>
 						</div>
@@ -329,8 +329,8 @@
 					for($i=1;$i<=4;$i++){
 						$pos_name = 'index_column'.$i
 					?>
-					<div class="photo_banner" <?php show_page_pos($pos_name,'column_full')?>>
-						<div class="photo_pg">
+					<div class="photo_banner">
+						<div class="photo_pg" <?php show_page_pos($pos_name,'column_full')?>>
 							<?php show_page_img();?>
 						</div>
 						<div class="photo_font">
