78d7fb9d9193f0a2d65458f5cd1132369c9bc6cf
justin <zhanghao@xun-ao.com>
2010-08-24 10:25:39 +0800

    1

--- admin/position/_column_index.php
+++ admin/position/_column_index.php
@@ -11,6 +11,7 @@ for($i=0;$i<4;$i++){
 		$ids[]=$item[0]->publisher;
 	}
 }
+var_dump($ids);
 //$items = $db->query("select publisher,b.name,b.nick_name,b.column_name,image_src,b.role_name from fb_news a left join fb_user b on a.publisher = b.id where role_name = '$role' and b.id != 72  group by publisher order by created_at desc limit 4");
 $len = count($items);
 $table = new table_class("fb_page_pos");
