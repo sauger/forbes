0b31ad0852742a4e13719307c52adf404c26971b
justin <zhanghao@xun-ao.com>
2010-08-24 10:55:11 +0800

    news

---
+++ news.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once( dirname(__FILE__) .'/frame.php');
+$db = get_db();
+$news = $db->query("select t1.title,t2.nick_name,t1.author from fb_news t1 join fb_user t2 on t1.publisher=t2.id where t1.created_at>'2010-8-1 00:00:00' and t1.created_at<'2010-8-23 23:59:59'");
+?>
+<table style="width:900px; float:left; display:inline">
+<tr>
+<td width="60%">标题</td><td width="20%">作者</td><td width="20%">上传者</td>
+</tr>
+<?php foreach($news as $news){?>
+<tr>
+<td><?php echo $news->title;?></td><td><?php echo $news->author;?></td><td><?php echo $news->publisher;?></td>
+</tr>
+<?php }?>
+</table>
