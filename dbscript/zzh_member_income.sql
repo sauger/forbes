4e55a21a4c5b0fc246772e62a471808ad5d3ffef
justin <zhanghao@xun-ao.com>
2010-08-31 10:37:46 +0800

    1

---
+++ dbscript/zzh_member_income.sql
@@ -0,0 +1,8 @@
+CREATE TABLE  `forbes`.`zzh_member_income` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sign_id` int(10) unsigned NOT NULL,
+  `year` int(10) unsigned NOT NULL,
+  `income` int(10) unsigned NOT NULL,
+  `type` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8
