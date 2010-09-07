f412d1395a0c27b842723193354cdda11a4c31c2
justin <zhanghao@xun-ao.com>
2010-09-07 14:55:31 +0800

    index

--- index.php
+++ index.php
@@ -239,9 +239,9 @@
 						for($i=0;$i<3;$i++){
 						$nid = '('.implode(',',$ids).')';
 						if($ids){
-							$item = $db->query("select t1.*,t2.name,t2.image_src from fb_news t1 join fb_user t2 on t1.publisher=t2.id where t1.publisher not in $nid and t2.role_name='column_writer' order by t1.created_at desc;");
+							$item = $db->query("select t1.*,t2.name,t2.image_src from fb_news t1 join fb_user t2 on t1.publisher=t2.id where t1.publisher not in $nid and t2.role_name='column_writer' and (t1.copy_from is null or t1.copy_from=0) order by t1.created_at desc");
 						}else{
-							$item = $db->query("select t1.*,t2.name,t2.image_src from fb_news t1 join fb_user t2 on t1.publisher=t2.id where t2.role_name='column_writer' order by t1.created_at desc;");
+							$item = $db->query("select t1.*,t2.name,t2.image_src from fb_news t1 join fb_user t2 on t1.publisher=t2.id where t2.role_name='column_writer' and (t1.copy_from is null or t1.copy_from=0) order by t1.created_at desc");
 						}
 						$ids[]=$item[0]->publisher;
 						?>
@@ -324,9 +324,9 @@
 						for($i=0;$i<3;$i++){
 						$nid = '('.implode(',',$ids).')';
 						if($ids){
-							$item = $db->query("select t1.*,t2.name,t2.image_src from fb_news t1 join fb_user t2 on t1.publisher=t2.id where t1.publisher not in $nid and t2.role_name='column_editor' order by t1.created_at desc;");
+							$item = $db->query("select t1.*,t2.name,t2.image_src from fb_news t1 join fb_user t2 on t1.publisher=t2.id where t1.publisher not in $nid and t2.role_name='column_editor' and (t1.copy_from is null or t1.copy_from=0) order by t1.created_at desc;");
 						}else{
-							$item = $db->query("select t1.*,t2.name,t2.image_src from fb_news t1 join fb_user t2 on t1.publisher=t2.id where t2.role_name='column_editor' order by t1.created_at desc;");
+							$item = $db->query("select t1.*,t2.name,t2.image_src from fb_news t1 join fb_user t2 on t1.publisher=t2.id where t2.role_name='column_editor' and (t1.copy_from is null or t1.copy_from=0) order by t1.created_at desc;");
 						}
 						if($item){
 							$ids[]=$item[0]->publisher;
