d7882a24d449c31d48b0a7368a2f352e0fab49e1
justin <zhanghao@xun-ao.com>
2010-09-07 11:20:11 +0800

    ad

--- news.php
+++ news.php
@@ -3,7 +3,7 @@
 include_once( dirname(__FILE__) .'/frame.php');
 $db = get_db();
 
-$channel = $db->query("select * forbes_ad.fb_channel where id!=3");
+$channel = $db->query("select * from forbes_ad.fb_channel where id!=3");
 
 foreach($channel as $v){
 	$db->execute("insert into forbes_ad.fb_channel_banner (channer_id, banner_id) values ({$v->id},20)");
